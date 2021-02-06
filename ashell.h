370baf935f728851a48ebd06341f39446a1b2cde
Anthony Kung <hi@anth.dev>
2021-02-06 13:29:38 -0800

    Update at Sat, 06 Feb 2021 21:29:37 GMT

---
+++ ashell.h
@@ -0,0 +1,20 @@
+/****************************************************
+ *                                                  *
+ * Name: ashell.h                                   *
+ * Description: CS 344 Assignment 4                 *
+ *              Anthonian Shell                     *
+ * Date: Feburary 6, 2021                           *
+ * Created with <3 by: Anthony Kung                 *
+ * Author URL: https://anth.dev                     *
+ * License: Apache-2.0 (See /LICENSE.md)            *
+ *                                                  *
+ ****************************************************/
+
+#ifndef ASHELL
+#define ASHELL
+
+#include "anthonian.h"
+
+
+
+#endif
