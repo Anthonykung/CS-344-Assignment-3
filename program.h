ed2d453ba8b16ad5f7bf27d3a8c428bf89eebb6a
Anthony Kung <hi@anth.dev>
2021-02-06 13:35:10 -0800

    Update at Sat, 06 Feb 2021 21:35:10 GMT

---
+++ program.h
@@ -0,0 +1,20 @@
+/****************************************************
+ *                                                  *
+ * Name: program.h                                  *
+ * Description: CS 344 Assignment 4                 *
+ *              Program Manager                     *
+ * Date: Feburary 6, 2021                           *
+ * Created with <3 by: Anthony Kung                 *
+ * Author URL: https://anth.dev                     *
+ * License: Apache-2.0 (See /LICENSE.md)            *
+ *                                                  *
+ ****************************************************/
+
+#ifndef PROGRAM
+#define PROGRAM
+
+#include "anthonian.h"
+
+int main(int argc, char *argv[]);
+
+#endif
