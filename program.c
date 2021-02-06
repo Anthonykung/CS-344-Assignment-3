26130f04fb39e60bb752961d739b5f21e92db705
Anthony Kung <hi@anth.dev>
2021-02-06 13:34:46 -0800

    Update at Sat, 06 Feb 2021 21:34:46 GMT

---
+++ program.c
@@ -0,0 +1,21 @@
+/****************************************************
+ *                                                  *
+ * Name: program.c                                  *
+ * Description: CS 344 Assignment 4                 *
+ *              Program Manager                     *
+ * Date: Feburary 6, 2021                           *
+ * Created with <3 by: Anthony Kung                 *
+ * Author URL: https://anth.dev                     *
+ * License: Apache-2.0 (See /LICENSE.md)            *
+ *                                                  *
+ ****************************************************/
+
+#include "program.h"
+
+/**
+ * Program Manager Function
+ * Used to call functions from main
+ */
+int main(int argc, char *argv[]) {
+  return 0;
+}
